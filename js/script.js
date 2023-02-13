f4832ff4239a9b30e153f212e7bc6ad53a971d76
thypedy <84384293+thypedy@users.noreply.github.com>
2023-02-13 12:16:00 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots:true,
+    infinite: true,
+    speed: 300,
+    slideToShow: 4,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
